9078fd94c0eba7e3ccb5ee10877f6e80e3888fa9
dannyjackson <49250788+dannyjackson@users.noreply.github.com>
2019-07-19 13:05:46 -0600

    Update fst_slidingwindow.sh

--- fst_slidingwindow.sh
+++ fst_slidingwindow.sh
@@ -10,9 +10,11 @@ if [ $# -lt 1 ]
     [-o] Output directory for files
     [-p] Population 1 file
     [-q] Population 2 file
-    [-n] Project name, prefix for output files"
+    [-n] Project name, prefix for output files
+    [-w] Window size
+    [-s] Window step size (usually give the same number as for window to prevent overlap)"
   else
-    while getopts v:o:p:q:n:w: option
+    while getopts v:o:p:q:n:w:s: option
     do
     case "${option}"
     in
@@ -22,10 +24,11 @@ if [ $# -lt 1 ]
     q) pop2=${OPTARG};;
     n) name=${OPTARG};;
     w) window=${OPTARG};;
+    s) step=${OPTARG};;
     esac
     done
     
-  vcftools --vcf $dataset --weir-fst-pop $pop1 --weir-fst-pop $pop2 --fst-window-size $window --fst-window-step $window --out $outDir/$name
+  vcftools --vcf $dataset --weir-fst-pop $pop1 --weir-fst-pop $pop2 --fst-window-size $window --fst-window-step $step --out $outDir/$name
 
   sed -i 's/scaffold//g' $name.windowed.weir.fst
   sed -i 's/Scaffold//g' $name.windowed.weir.fst
